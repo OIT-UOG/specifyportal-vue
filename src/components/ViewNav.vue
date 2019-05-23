1bba134aca4175303e58b7114afa4e0d9d501193
Chovin <Chovin@users.noreply.github.com>
2019-05-23 10:11:20 +1000

    view nav to tab between them

---
+++ src/components/ViewNav.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+    <v-tabs>
+      <v-tab v-for="route in routes" :to="route.path">
+        {{ route.name }}
+      </v-tab>
+    </v-tabs>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      routes: this.$router.options.routes
+    }
+  }
+}
+</script>
