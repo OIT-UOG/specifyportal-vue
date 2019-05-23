165cd8693e157d4f64dafe1b969a3a6e01d11fad
Chovin <Chovin@users.noreply.github.com>
2019-05-23 10:10:42 +1000

    route to the different result views

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Results from './views/Results.vue';
 
 Vue.use(Router);
 
@@ -8,16 +8,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'results',
+      component: Results,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/gallery',
+      name: 'gallery',
       // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
+      // this generates a separate chunk (gallery.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "gallery" */ './views/Gallery.vue'),
     },
+    {
+      path: '/map',
+      name: 'map',
+      component: () => import(/* webpackChunkName: "map" */ './views/Map.vue'),
+    }
   ],
 });
