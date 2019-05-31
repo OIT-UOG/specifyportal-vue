9cac75eb5347981db8b889621a6559552feee3c0
Chovin <Chovin@users.noreply.github.com>
2019-05-31 19:20:38 +1000

    add rows per page

---
+++ src/views/Results.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="entries"
+    :rows-per-page-items="rowsPerPage"
+  >
+    <!-- hide-actions -->
+    <template v-slot:items="props">
+      <td
+        v-for="f of visibleCols"
+        :key=f.solrname
+        class="text-xs-right"
+      >{{ props.item[f.solrname] }}</td>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Results',
+  data () {
+    return {
+      rowsPerPage: [20,50,100]
+    }
+  },
+  computed: {
+    headers () {
+      return this.visibleCols.map((h) => {
+        return {
+          text: h.colname,
+          value: h.solrname
+        }
+      })
+    },
+    ...mapGetters(['moreToQuery', 'entries', 'numFound', 'colAttrs', 'visibleCols'])
+  }
+}
+</script>
+
+<style>
+  tr:nth-of-type(even) {
+    background-color: #e8edf1;
+  }
+</style>
+
