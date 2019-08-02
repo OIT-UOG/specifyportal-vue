c950a4e20fbd10b6494df106232d8fa18cfaff6a
Chovin <Chovin@users.noreply.github.com>
2019-08-02 16:39:37 +1000

    string filter

---
+++ src/components/StringFilter.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<v-text-field
+			label=" Search"
+			v-model="stuff"
+			@keydown.enter="submit"
+			single-line
+			hide-details
+			full-width
+		></v-text-field>
+	</div>
+</template>
+
+<script>
+import { mapActions, mapGetters, mapState } from 'vuex'
+
+export default {
+	props: {
+		title: String,
+		colkey: String
+	},
+	data () {
+		return {
+			stuff: ""
+		}
+  	},
+	methods: {
+		submit(e) {
+			this.addSearchTerm({ field: this.colkey, search: this.stuff })
+			this.stuff = ""
+			this.runNewQuery()
+		},
+		...mapActions(['addSearchTerm', 'runNewQuery'])
+	}
+}
+</script>
